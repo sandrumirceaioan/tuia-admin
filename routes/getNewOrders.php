e86e350aacb48dbe3a9b07ab963082e13fa59941
SandruMirceaIoan <sandrumirceaioan@gmail.com>
2017-03-10 15:23:40 +0200

    admin-order-notifications

--- routes/getNewOrders.php
+++ routes/getNewOrders.php
@@ -1,14 +1,10 @@
-<?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-
-include('connect.php');
-
+<?phpheader('Content-Type: application/json');
+error_reporting(-1);
+ini_set('display_errors', 'On');
+include('connect.php');
+
 $orders = mysqli_query($conexiune, "SELECT * FROM orders WHERE osolved=0");
-
-            $line = mysqli_num_rows($orders);
-			$arr['ord'] = $line;
-			
-echo json_encode($arr);
+        $line = mysqli_num_rows($orders);			  $arr['ord'] = $line;
+        http_response_code(200);        echo json_encode($arr);
 
 ?>
